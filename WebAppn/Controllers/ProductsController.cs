5f3e54c10082217b366939b692a7712b23ee0174
Akhil Goel <akhil.goel@nagarro.com>
2020-08-29 17:38:53 +0530

    added new products controller

---
+++ WebAppn/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppn.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : Controller
+    {
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			return new string[] { "p1", "p2", "p3" };
+		}
+	}
+}
